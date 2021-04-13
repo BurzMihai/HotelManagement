dca1070f39e08ef0e0dd0436462d96f7672b971e
BurzMihai <80482174+BurzMihai@users.noreply.github.com>
2021-04-13 20:10:52 +0300

    Adaugare client, reservations, room model si modificare room si reservation domain.
    Adaugare client, reservations, room model si modificare room si reservation domain.

--- Model/client_model.py
+++ Model/client_model.py
@@ -1,12 +1,49 @@
-from sqlalchemy import text
-
+from sqlalchemy.orm import sessionmaker
+from Domain.client import Client
 from View.utils import engine
 
 
 class ClientsModel:
     def __init__(self):
         self.__engine = engine
+        self.__my_session = sessionmaker(bind=engine)()
 
+    def read_client(self):
+        self.__my_session.query(Client).all()
 
+    def create_client(self, client_id, first_name, last_name, email):
+        self.__my_session.add(
+            Client(
+                client_id=client_id,
+                first_name=first_name,
+                last_name=last_name,
+                email=email
+            )
+        )
+        self.__my_session.commit()
 
+    def update_client(self, client_id, first_name=None, last_name=None, email=None):
+        my_client = self.__my_session.query(Client).filter_by(client_id=client_id).first()
+        if my_client:
+            self.__my_session.query(
+                Client
+            ).filter_by(
+                client_id=client_id
+            ).update({
+                'firstname': f'{first_name or my_client.first_name}',
+                'lastname': f'{last_name or my_client.last_name}',
+                'email': f'{email or my_client.email}'
+            })
+            self.__my_session.commit()
 
+    def delete_client(self):
+        self.__my_session.query(Client).filter_by(client_id=client_id).delete()
+        self.__my_session.commit()
+
+    def find_by_id(self, client_id):
+        my_client = self.__my_session.query(Client).filter_by(client_id=client_id).first()
+        return my_client
+
+    def id_exists(self, client_id):
+        my_client = self.__my_session.query(Client).filter_by(client_id=client_id).first()
+        return True if my_client else False
---
+++ Model/reservations_model.py
@@ -0,0 +1,50 @@
+from sqlalchemy.orm import sessionmaker
+
+from Domain.reservation import Reservation
+from View.utils import my_session, engine
+
+
+class ClientsModel:
+    def __init__(self):
+        self.__my_session = sessionmaker(bind=engine)()
+        self.__engine = engine
+
+    def read_room(self):
+        self.__my_session.query(Reservation).all()
+
+    def create_reservation(self, reservations_id, client_id, room_number, start_date, end_date):
+        self.__my_session.add(
+            Reservation(
+                reservations_id=reservations_id,
+                client_id=client_id,
+                room_number=room_number,
+                start_date=start_date,
+                end_date=end_date
+            )
+        )
+        self.__my_session.commit()
+
+    def update_reservation(self, reservations_id, start_date, end_date):
+        my_reservation = self.__my_session.query(Reservation).filter_by(reservations_id=reservations_id).first()
+        if my_reservation:
+            self.__my_session.query(
+                Reservation
+            ).filter_by(
+                reservations_id=reservations_id
+            ).update({
+                'start_date': f'{start_date or my_reservation.start_date}',
+                'end_date': f'{end_date or my_reservation.end_date}',
+            })
+            self.__my_session.commit()
+
+    def delete_reservation(self, reservations_id):
+        self.__my_session.query(Reservation).filter_by(reservations_id=reservations_id).delete()
+        self.__my_session.commit()
+
+    def find_by_reservation_id(self, reservations_id):
+        my_reservation = self.__my_session.query(Reservation).filter_by(reservations_id=reservations_id).first()
+        return my_reservation
+
+    def reservation_exists(self, reservations_id):
+        my_reservation = self.__my_session.query(Reservation).filter_by(reservations_id=reservations_id).first()
+        return True if my_reservation else False
